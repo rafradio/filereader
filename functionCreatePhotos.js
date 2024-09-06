392fb7c12d6d194d7232911f7e45a1a1c69487d1
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-09-06 15:55:40 +0300

    functionCreatePhotos.js

--- functionCreatePhotos.js
+++ functionCreatePhotos.js
@@ -17,24 +17,83 @@ allPhotosField.forEach((item, index) => {
             let idName = targetQuestion.id;
             if (!(idName.includes("qwrap"))) checkFlag = false;
         }
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute("class", "picture-front-block");
+//        newDiv.style.width = "700px";
+//        newDiv.style.height = "300px";
+//        newDiv.style.display = "flex";
         console.log("Добавляем вопрос", selectQtrArr);
         const myImage = new Image();
         myImage.height = 300;
+//        myImage.width = 400;
+        myImage.style.objectFit = "contain";
         let selectedFile = event.target.files[0];
         let reader = new FileReader();
         reader.onload = function(event) {
             myImage.src = event.target.result;
+            item.value = "";
           };
         reader.readAsDataURL(selectedFile);
         let newSelect = document.createElement('select');
-        newSelect.style.width = '200px';
+        newSelect.setAttribute("class", "select-for-photo");
+        let optFirst = document.createElement('option');
+        optFirst.value = 0;
+        optFirst.text = "Выберите вопрос";
+        newSelect.appendChild(optFirst);
         selectQtrArr.forEach((value, key) => {
             let opt = document.createElement('option');
             opt.value = key;
             opt.text = value;
             newSelect.appendChild(opt);
         });
-        item.parentNode.appendChild(myImage);
-        item.parentNode.appendChild(newSelect);
+//        newSelect.onchange = selectChange;
+        let newButton = document.createElement('button');
+        newButton.innerHTML = "Удалить";
+        newButton.setAttribute("class", "delete-for-photo");
+        newDiv.appendChild(myImage);
+        newDiv.appendChild(newButton);
+        newDiv.appendChild(newSelect);
+        item.parentNode.appendChild(newDiv);
+        let newItem = item.parentNode.lastElementChild;
+        
+        console.log("Поиск ребенка = ", newItem.lastElementChild);
+        let self = newItem.lastElementChild;
+        let newItemButton = self.previousElementSibling;
+        newItem.lastElementChild.onchange = selectChange.bind(self);
+//        newItem.lastElementChild.onchange = () => {selectChange();};
+        newItemButton.onclick = () => {newItemButton.parentNode.remove();};
+        
     }
 });
+
+const selectChange = function() {
+    console.log("Value вопроса = ", this.value);
+    let checkFleBlockPhoto = this.value + "-photos";
+    let questionElem = document.getElementById(this.value);
+    let parentPhotoNode = questionElem;
+    if (questionElem.lastChild.className == "wrap-picture-front") {
+        parentPhotoNode = questionElem.lastChild;
+        checkPhotos(parentPhotoNode,this.parentNode.firstChild.src);
+    } else {
+        parentPhotoNode = document.createElement('div');
+        parentPhotoNode.setAttribute("class", "wrap-picture-front");
+        questionElem.appendChild(parentPhotoNode);
+    }
+    let clone = this.parentNode.cloneNode(true);
+    parentPhotoNode.appendChild(clone);
+
+//    let element = questionElem.lastElementChild.lastElementChild;
+    let element = parentPhotoNode.lastElementChild.lastElementChild;
+    element.onchange = selectChange;
+    element.previousElementSibling.onclick = () => {element.parentNode.remove();};
+    this.parentNode.remove();
+    let imgs = document.images;
+//    console.log(imgs[0].src);
+}
+
+let checkPhotos = function(parentPhotoNode, photoSrc) {
+    let arrIm = Array.prototype.map.call(parentPhotoNode.children, elem => elem.firstChild.src).filter(imgSrc => imgSrc == photoSrc);
+    console.log(arrIm.length);
+//    let array = Array.prototype.map.call(parentPhotoNode.images, img => img.src).filter(imgSrc => imgSrc == photoSrc);
+//    console.log(array.length);
+}
